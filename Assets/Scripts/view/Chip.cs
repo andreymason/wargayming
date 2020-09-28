44b3ffd813ec14d58098368a184e900a769838df
Dan <kolisnychenko.dan2019@gmail.com>
2020-09-28 21:19:40 +0300

    aded support for spawning chips + Adequate directory structure

---
+++ Assets/Scripts/view/Chip.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chip : MonoBehaviour
+{
+    Color color = Color.White;
+
+    public void Flip(){
+        gameObject.transform.Rotate(180f,0f,0f,Space.Self);
+        color = (Color)(((int)color + 1)%2);
+    }
+}
---
+++ Assets/Scripts/view/ChipSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChipSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject chipPrefab;
+
+    const float FieldSize = 0.625f;
+    const float coordXOffset = -2.175f;
+    const float coordYOffset = 0.15f;
+    const float coordZOffset = -2.175f;
+
+    //Spawns Chip in the given coordinates and with given color
+    public void SpawnChip(Alpha sym, int num, Color color){
+        if(num <= 0 || num > 8 ) throw new System.Exception();
+
+        GameObject chip = Instantiate<GameObject>(chipPrefab);
+        chip.transform.position = new Vector3(
+            coordXOffset + (float)(num - 1) * FieldSize,
+            coordYOffset,
+            coordZOffset + (float)sym * FieldSize
+        );
+        if(color == Color.Black) chip.GetComponent<Chip>().Flip();
+    }
+}
