d091235f019808789f5a5680b4bb747dc5dccf86
KirillSSS <63448843+KirillSSS@users.noreply.github.com>
2020-10-03 16:03:03 +0300

    Это новое меню с работающими кнопками пвп и пве, но они не кидают на разные настройки игры пока (просто заменить этим файлом существующий уже файл Menu)
    Это новое меню с работающими кнопками пвп и пве, но они не кидают на разные настройки игры пока (просто заменить этим файлом существующий уже файл Menu)

--- Menu/CanvasButton.cs
+++ Menu/CanvasButton.cs
@@ -1,12 +1,27 @@
-﻿using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class CanvasButton : MonoBehaviour
 {
-    public string sceneName;
-     
+    public GameObject PvP;
+    public GameObject PvE;
+    public int _sceneNumber2;
+
     public void RestartGame(int _sceneNumber)
     {
-        SceneManager.LoadScene(_sceneNumber);
+        if (PvP.activeSelf)
+        {
+            SceneManager.LoadScene(_sceneNumber);
+            //_sceneNumber = scene "gay"
+            //What should be done when choosing pvp
+        }
+        else
+        { 
+            SceneManager.LoadScene(_sceneNumber2);
+            //_sceneNumber2 = another random scene 
+            //What should be done when choosing pve
+        }
     }
 }
