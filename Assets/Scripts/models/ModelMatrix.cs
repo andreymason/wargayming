945e2671c30c5a1b21aaed24bfa243c3b90a782e
andreymason <andreymason47@gmail.com>
2020-10-03 16:28:01 +0300

    РўРµРїРµСЂСЊ РјРѕР¶РЅРѕ СЂР°СЃСЃС‚Р°РІР»СЏС‚СЊ С„РёС€РєРё Р±РµР»С‹Рµ Рё С‡РµСЂРЅС‹Рµ

---
+++ Assets/Scripts/models/ModelMatrix.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelMatrix : MonoBehaviour{
+
+    public static int [,] mainMatrix;
+    
+    void Start(){
+        mainMatrix = new int [8,8];
+        // foreach(int value in mainMatrix){
+        //     print(value);
+        // }
+    }
+
+    void Update (){
+ 
+    }
+
+// adds in mainMatrix -1 if color == Black
+//                     1 if color == White
+    public void AddValueToMainMatrix(Alpha sym, int num, Color color){
+        if(color == Color.Black)
+            mainMatrix[num - 1, (int)sym] = -1;
+        else
+            mainMatrix[num - 1, (int)sym] =  1;
+    }
+}
