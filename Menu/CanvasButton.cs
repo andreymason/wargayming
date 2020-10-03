355b5c96e96f05ab0ea1847b9fb4823bb35bbe04
KirillSSS <63448843+KirillSSS@users.noreply.github.com>
2020-10-03 22:42:56 +0300

    Вот анимация ))
    Вот анимация ))

--- Menu/CanvasButton.cs
+++ Menu/CanvasButton.cs
@@ -7,21 +7,51 @@ public class CanvasButton : MonoBehaviour
 {
     public GameObject PvP;
     public GameObject PvE;
+    public GameObject Start;
+    public GameObject Exit;
+    public GameObject PvPButton;
+    public GameObject PvEButton;
     public int _sceneNumber2;
+    public int _sceneNumber;
+    public Animation Anim;
+    public float time;
+    private int x;
 
-    public void RestartGame(int _sceneNumber)
+    public void RestartGame()
     {
         if (PvP.activeSelf)
         {
-            SceneManager.LoadScene(_sceneNumber);
+            x = 1;
             //_sceneNumber = scene "gay"
             //What should be done when choosing pvp
         }
         else
-        { 
-            SceneManager.LoadScene(_sceneNumber2);
+        {
+            x = 0;
             //_sceneNumber2 = another random scene 
             //What should be done when choosing pve
+        }
+
+        PvP.SetActive(false);
+        PvE.SetActive(false);
+        Start.SetActive(false);
+        Exit.SetActive(false);
+        PvPButton.SetActive(false);
+        PvEButton.SetActive(false);
+        
+        Anim.Play("New Animation");
+        Invoke("Game", time);
+    }
+
+    private void Game()
+    {
+        if (x == 1)
+        {
+            SceneManager.LoadScene(_sceneNumber);
+        }
+        else
+        {
+            SceneManager.LoadScene(_sceneNumber2);
         }
     }
 }
