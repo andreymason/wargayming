a094d698fcfd6a099d8c3ca8efd215d6fb8c464d
andreymason <andreymason47@gmail.com>
2020-10-03 14:35:58 +0300

    РЈРґР°Р»РёР» РЅРµРЅСѓР¶РЅС‹Рµ С„Р°Р№Р»С‹ Рё СЃС†РµРЅС‹, Р·Р°РєРёРЅСѓР» РјРµРЅСЋ

---
+++ Assets/Scenes/Menu/CanvasButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CanvasButton : MonoBehaviour
+{
+    public string sceneName;
+     
+    public void RestartGame(int _sceneNumber)
+    {
+        SceneManager.LoadScene(_sceneNumber);
+    }
+}
---
+++ Assets/Scenes/Menu/ExitButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    public string sceneName;
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
