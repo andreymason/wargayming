e671855c10b90595ec1a82bcf565b94c63f202f1
Dan <kolisnychenko.dan2019@gmail.com>
2020-10-07 00:09:24 +0300

    yay the logic is almost done, I see the end, soon, guys, I see ...

--- Assets/Scripts/models/ModelMatrix.cs
+++ Assets/Scripts/models/ModelMatrix.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,13 +16,14 @@ public class ModelMatrix : MonoBehaviour{
         mainMatrix[4,4] = 1;
         mainMatrix[3,4] = -1;
         mainMatrix[4,3] = -1;
-        cp.SpawnChip((Alpha)3,4,Color.White);
-        cp.SpawnChip((Alpha)4,5,Color.White);
-        cp.SpawnChip((Alpha)3,5,Color.Black);
-        cp.SpawnChip((Alpha)4,4,Color.Black);
+        cp.SpawnChip(Alpha.D,4,Color.White);
+        cp.SpawnChip(Alpha.E,5,Color.White);
+        cp.SpawnChip(Alpha.D,5,Color.Black);
+        cp.SpawnChip(Alpha.E,4,Color.Black);
     }
 
-    public bool AddValueToMainMatrix(Alpha sym, int num, Color color){
+    public bool AddValueToMainMatrix(Alpha sym, int num, Color color)
+    {
         PossibleTurnsMatrix(color);
         if(turn[num - 1, (int)sym]){
             if(color == Color.Black){
@@ -30,6 +32,9 @@ public class ModelMatrix : MonoBehaviour{
             else{
                 mainMatrix[num - 1, (int)sym] =  1;
             }
+
+            turn[num - 1, (int) sym] = false;
+            
             cp.SpawnChip(sym,num,color);
             AudioManager.Play(AudioClipName.chip);
             return true;
@@ -37,169 +42,70 @@ public class ModelMatrix : MonoBehaviour{
         return false;
     }
     
+    //fills the possible turns matrix for the given player color
     public void PossibleTurnsMatrix (Color color)
-	{
-		sbyte igrock;
-		if(color ==Color.White)
-			igrock = 1;
-		else
-			igrock = -1;
-
-		//происзодит заполнение матрицы возможных ходов
-		for (byte i = 0; i < 8; i++)
+    {
+        for (byte i = 0; i < 8; i++)
 			for (byte j = 0; j < 8; j++)
-			{
-				if (igrock == mainMatrix [i, j]) 
-				{
-					est_li_hod (mainMatrix [i, j], i, j);
-				} 
-				else 
-				{
-					continue;
-				}
-			}
+            {
+                if (mainMatrix[i, j] == 0)
+                {
+                    IsTurnPossibleUpdate(color == Color.White? 1 : -1, i, j);
+                }
+                else continue;
+            }
 	}
 
-	void est_li_hod (int igroc_color, int num, int sym)
-	{
-		//вверх
-		if (num > 0 && igroc_color == (-mainMatrix [num - 1, sym])) //проверка, есть ли сверху фишка противоположного цвета (и фишка должна біть не на краю доски)
-		{ 
-			for (int i = num - 1; i >= 0; i--) //иду вверз от текущей позиции
-			{
-				if (igroc_color == (mainMatrix [i, sym])) //если встречаю фишку своего цвета до того, как была найдена пустая клетка, походить сверху не выйдет 
-				{
-					break;
-				}
-				else if (mainMatrix [i, sym] == 0) //если после всех фишек противоположного цвета есть пустая клетка, значит в неё есть возможность походить
-				{
-					turn [i, sym] = true; //по координатам клетки ставлю тру (ходить туда можно)
-					break;
-				}
-			}
-		} else {} //если не ставить иногда начинается какая то катовсия с ифами, на всякий пожарній крч
-
-		//остальное аналогично, меняется только сторона, "куда я иду проверять"
-
-		//вверх-вправо
-		if (num > 0 && sym < 7 && igroc_color == (-mainMatrix [num - 1,sym + 1]))
-		{
-			for (int i = num - 1, j = sym + 1; i >= 0 && j < 8; i--, j++) {
-				if (igroc_color == (mainMatrix [i, j])) 
-				{ 
-					break;
-				}
-				else if (mainMatrix [i, j] == 0) 
-				{ 
-					turn [i, j] = true; 
-					break;
-				}
-			}
-		} else {}
-
-
-		//вправо
-		if (sym < 7 && igroc_color == (-mainMatrix [num,sym + 1]))
-		{
-			for (int i = sym + 1; i < 8; i++) 
-			{
-				if (igroc_color == (mainMatrix [num, i]))
-				{
-					break;
-				}
-				else if (mainMatrix [num, i] == 0) 
-				{
-					turn [num, i] = true; 
-					break;
-				}
-			}
-		} else {}
-
-
-		//вниз-вправо
-		if (num < 7 && sym < 7 && igroc_color == (-mainMatrix [num + 1,sym + 1]))
-		{
-			for (int i = num + 1, j = sym + 1; i < 8 && j < 8; i++, j++) {
-				if (igroc_color == (mainMatrix [i, j]))
-				{
-					break;
-				}
-				else if (mainMatrix [i, j] == 0) 
-				{ 
-					turn [i, j] = true; 
-					break;
-				} 
-			}
-		} else {}
-
-
-		//вниз
-		if (num < 7 && igroc_color == (-mainMatrix [num + 1,sym]))
-		{
-			for (int i = num + 1; i < 8; i++) 
-			{
-				if (igroc_color == (mainMatrix [i, sym])) 
-				{
-					break;
-				}
-				else if (mainMatrix [i, sym] == 0)
-				{
-					turn [i, sym] = true;
-					break;
-				}
-			}
-		} else {}
-
-
-		//вниз-влево
-		if (num < 7 && sym > 0 && igroc_color == (-mainMatrix [num + 1,sym - 1]))
-		{
-			for (int i = num + 1, j = sym - 1; i < 8 && j >= 0; i++, j--) {
-				if (igroc_color == (mainMatrix [i, j]))
-				{
-					break;
-				}
-				else if (mainMatrix [i, j] == 0) 
-				{
-					turn [i, j] = true; 
-					break;
-				}
-			}
-		} else {}
-
-
-		//влево
-		if (sym > 0 && igroc_color == (-mainMatrix [num,sym - 1]))
-		{
-			for (int i = sym - 1; i >= 0; i--) 
-			{
-				if (igroc_color == (mainMatrix [num, i]))
-				{
-					break;
-				}
-				else if (mainMatrix [num, i] == 0) 
-				{
-					turn [num, i] = true;
-					break;
-				}
-			} 
-		} else {}
-
-
-		//вверх-влево
-		if (num > 0 && sym > 0 && igroc_color == (-mainMatrix [num - 1,sym - 1]))
-		{
-			for (int i = num - 1, j = sym - 1; i >= 0 && j >= 0; i--, j--) {
-				if (igroc_color == (mainMatrix [i, j]))
-				{
-					break;
-				}
-				else if (mainMatrix [i, j] == 0) 
-				{
-					turn [i, j] = true; 
-					break;
-				}
-			}
-		} else {}
+    /// <summary>
+    /// checks whether the turn move is possible
+    /// </summary>
+    /// <param name="playerColor"></param>
+    /// <param name="num"></param>
+    /// <param name="sym"></param>
+	void IsTurnPossibleUpdate (int playerColor, int num, int sym)
+    {
+        turn[num, sym] = false;
+        foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+        {
+            int skaX = directionToCoordinate(direction, 'x');
+            int skaY = directionToCoordinate(direction, 'y');
+            for (int i = num, j = sym; (i + 2 * skaX) >= 0 && (i + 2 * skaX) < 8  && 
+                                       (j + 2 * skaY) >= 0 && (j + 2 * skaY)< 8 && 
+                                       (mainMatrix[i +  skaX, j + skaY] != playerColor) &&
+                                       (mainMatrix[i +  skaX, j + skaY] != 0);)
+            {
+                j += skaY;
+                i += skaX;
+                if (mainMatrix[i + skaX, j + skaY] == playerColor)
+                {
+                    turn[i - skaX, j - skaY] = true;
+                }
+            }
+        }
 	}
+
+    /// <summary>
+    /// converts direction to sutable for logic functions view
+    /// Avilable axes are 'x' and 'y'
+    /// </summary>
+    /// <param name="direction"></param>
+    /// <param name="axis"></param>
+    int directionToCoordinate(Direction direction, char axis)
+    {
+        if (axis == 'x')
+        {
+            if ((int) direction % 4 == 0) return 0;
+            else if ((int) direction < 4) return 1;
+            else return -1;
+        }
+        else if (axis == 'y')
+        {
+            if ((int) direction % 4 == 2) return 0;
+            else if ((int) direction % 6 < 2) return 1;
+            else return -1;
+        }
+        else throw new ArgumentOutOfRangeException();
+    }
+    
+
 }
--- Assets/Scripts/models/TurnManager.cs
+++ Assets/Scripts/models/TurnManager.cs
@@ -19,15 +19,14 @@ public class TurnManager : MonoBehaviour{
             }
         }
         if(color != userColor){
-            AiTurn(color);
+            AiTurn();
             color = (Color)(((int)color + 1)%2);
         }
     }
-
-    private void AiTurn(Color color){
+  
+    private void AiTurn(){
         modMatObj.PossibleTurnsMatrix(color);
         List<int[]> posTurns = new List<int[]>();
-        MatrixDebug();
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 8; j++){
                 if(modMatObj.turn[i,j]){
@@ -40,28 +39,9 @@ public class TurnManager : MonoBehaviour{
         }
 
         int[] randomMove = posTurns[Random.Range(0,posTurns.Count)];
-        
-        Debug.Log("AiTurn: " + (Alpha)randomMove[1] + " " + (randomMove[0]+1));
-        modMatObj.AddValueToMainMatrix((Alpha)randomMove[1],randomMove[0] + 1, color);
-    }  
 
-    void MatrixDebug(){
-        string a = "A          ";
-        a += "B         ";
-        a += "C         ";
-        a += "D         ";
-        a += "E         ";
-        a += "F         ";
-        a += "G         ";
-        a += "H         \n";
-        for(int i = 0; i < 8; i++){
-            a += (i+1) + " ";
-            for(int j = 0; j < 8; j++){
-                a += (modMatObj.turn[i,j].ToString() + " ").PadRight(5,' ');
-            }
-            a += "\n";
-        }
-        Debug.Log(a); 
+        string debug = "AiTurn: " + (Alpha)randomMove[1] + " " + (randomMove[0]+1);
+        modMatObj.AddValueToMainMatrix((Alpha)randomMove[1],randomMove[0] + 1, color);
     }
 }
 
