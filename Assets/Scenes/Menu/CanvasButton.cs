389be565ace9ec14e19d187b3f88c118e82b84b1
andreymason <andreymason47@gmail.com>
2020-10-07 09:57:35 +0300

    Игра + новое меню + надо починить анимацию

--- Assets/Scenes/Menu/CanvasButton.cs
+++ Assets/Scenes/Menu/CanvasButton.cs
@@ -7,21 +7,43 @@ public class CanvasButton : MonoBehaviour
 {
     public GameObject PvP;
     public GameObject PvE;
+    public GameObject Canvas;
     public int _sceneNumber2;
+    public int _sceneNumber;
+    public Animation Anim;
+    public float time;
+    private int x;
 
-    public void RestartGame(int _sceneNumber)
+    public void RestartGame()
     {
         if (PvP.activeSelf)
         {
-            SceneManager.LoadScene(_sceneNumber);
+            x = 1;
             //_sceneNumber = scene "gay"
             //What should be done when choosing pvp
         }
         else
-        { 
-            SceneManager.LoadScene(_sceneNumber2);
+        {
+            x = 0;
             //_sceneNumber2 = another random scene 
             //What should be done when choosing pve
+        }
+
+        Canvas.SetActive(false);
+
+        Anim.Play("New Animation");
+        //Invoke("Game", time);
+    }
+
+    private void Game()
+    {
+        if (x == 1)
+        {
+            //Тут будет логика ПвП
+        }
+        else
+        {
+            //Тут будет логика ПвЕ
         }
     }
 }
---
+++ Assets/Scenes/Menu/Music.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public GameObject Line;
+    public GameObject Sound;
+
+    public void MusicAppear()
+    {
+        if (Sound.activeSelf)
+        {
+            Line.SetActive(true);
+            Sound.SetActive(false);
+        }
+        else
+        {
+            Line.SetActive(false);
+            Sound.SetActive(true);
+        }
+    }
+}
